3b609df36236f5ccedafe787112204e66131e9de
Marc Lacoursière <marc@roosoft.com>
2024-09-18 15:29:21 -0400

    workig but minimal state

--- modules/system/minio/default.nix
+++ modules/system/minio/default.nix
@@ -7,14 +7,14 @@
   cfg = config.roopkgs.system.minio;
 in {
   imports = [
-    ./kes.nix
+    # ./kes.nix
   ];
 
   options = with lib; {
     roopkgs.system.minio = {
       enable = mkEnableOption "minio";
 
-      dataPort = mkOption {
+      listenPort = mkOption {
         type = types.port;
         default = 9000;
       };
@@ -34,55 +34,64 @@ in {
   config = lib.mkIf cfg.enable {
     networking = {
       firewall = {
-        allowedTCPPorts = [cfg.dataPort cfg.consolePort];
+        allowedTCPPorts = [cfg.listenPort cfg.consolePort];
       };
     };
+
+    users.groups.minio = {};
 
     users.users.minio = {
+      isSystemUser = true;
       extraGroups = ["kes"];
+      group = "minio";
     };
 
-    systemd.services.minio = {
-      description = "MinIO server";
-      wants = ["network-online.target"];
-      after = ["kes.service"];
+    systemd = {
+      tmpfiles.rules = [
+        "d ${cfg.workingDirectory} 750 minio minio"
+        "d ${cfg.workingDirectory}/config 750 minio minio"
+        "d ${cfg.workingDirectory}/data 750 minio minio"
+      ];
 
-      environment = let
-        kesConfigFolder = "/var/lib/kes";
-        caFolder = "/var/lib/minio/config/CAs";
-      in {
-        SSL_CERT_DIR = kesConfigFolder;
+      services.minio = {
+        description = "MinIO server";
+        wants = ["network-online.target"];
+        after = ["kes.service"];
 
-        MINIO_KMS_KES_ENDPOINT = "https://127.0.0.1:7373";
+        # environment = let
+        #   kesConfigFolder = "/var/lib/kes";
+        #   caFolder = "/var/lib/minio/config/CAs";
+        # in {
+        #   SSL_CERT_DIR = kesConfigFolder;
 
-        MINIO_KMS_KES_CERT_FILE = "${caFolder}/kes.crt";
-        MINIO_KMS_KES_KEY_FILE = "${caFolder}/kes.key";
+        #   MINIO_KMS_KES_ENDPOINT = "https://127.0.0.1:7373";
 
-        MINIO_KMS_KES_KEY_NAME = "my-key-2";
+        #   MINIO_KMS_KES_CERT_FILE = "${caFolder}/kes.crt";
+        #   MINIO_KMS_KES_KEY_FILE = "${caFolder}/kes.key";
 
-        MINIO_KMS_KES_CAPATH = "${kesConfigFolder}/public.crt";
+        #   MINIO_KMS_KES_KEY_NAME = "my-key-2";
+
+        #   MINIO_KMS_KES_CAPATH = "${kesConfigFolder}/public.crt";
+        # };
+
+        serviceConfig = {
+          User = "minio";
+          ExecStart = ''${pkgs.minio}/bin/minio server datai --address ":${toString cfg.listenPort}" --console-address ":${toString cfg.consolePort}"'';
+
+          WorkingDirectory = cfg.workingDirectory;
+          Type = "simple";
+          Restart = "always";
+          TimeoutSec = 120;
+          RestartSec = 30;
+          KillMode = "process";
+
+          PrivateTmp = true;
+          PrivateDevices = true;
+          MemoryDenyWriteExecute = true;
+        };
+
+        wantedBy = ["multi-user.target"];
       };
-
-      serviceConfig = {
-        User = "kes";
-        ExecStart = ''${pkgs.minio}/bin/minio server \
-          --address ":${toString cfg.dataPort}" \
-          --console-address ":${toString cfg.consolePort}"
-        '';
-
-        WorkingDirectory = cfg.workingDirectory;
-        Type = "simple";
-        Restart = "always";
-        TimeoutSec = 120;
-        RestartSec = 30;
-        KillMode = "process";
-
-        PrivateTmp = true;
-        PrivateDevices = true;
-        MemoryDenyWriteExecute = true;
-      };
-
-      wantedBy = ["multi-user.target"];
     };
   };
 }
