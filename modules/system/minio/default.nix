fca9ff779d4328b9fe753fbc74bff58d5cbbabe1
Marc Lacoursière <marc@roosoft.com>
2024-09-18 20:25:38 -0400

    running two minio servers on the same machine

--- modules/system/minio/default.nix
+++ modules/system/minio/default.nix
@@ -3,40 +3,49 @@
   pkgs,
   config,
   ...
-}: let
-  cfg = config.roopkgs.system.minio;
-in {
+}: {
   imports = [
     # ./kes.nix
   ];
 
   options = with lib; {
-    roopkgs.system.minio = {
-      enable = mkEnableOption "minio";
+    roopkgs.system.minio = mkOption {
+      default = {};
+      type = types.attrsOf (types.submodule {
+        options = {
+          enable = mkEnableOption "minio";
 
-      listenPort = mkOption {
-        type = types.port;
-        default = 9000;
-      };
+          listenPort = mkOption {
+            type = types.port;
+            default = 9000;
+          };
 
-      consolePort = mkOption {
-        type = types.port;
-        default = 9001;
-      };
+          consolePort = mkOption {
+            type = types.port;
+            default = 9001;
+          };
 
-      workingDirectory = mkOption {
-        type = types.path;
-        default = "/var/lib/minio";
-      };
+          workingDirectory = mkOption {
+            type = types.path;
+            default = "/var/lib/minio";
+          };
+        };
+      });
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    networking = {
-      firewall = {
-        allowedTCPPorts = [cfg.listenPort cfg.consolePort];
-      };
-    };
+  config = let
+    filteredCfgs = lib.attrsets.filterAttrs (_: cfg: cfg.enable) config.roopkgs.system.minio;
+  in {
+    networking =
+      lib.attrsets.mapAttrs' (name: cfg: {
+        name = "firewall";
+        value = {
+          allowedTCPPorts = [cfg.listenPort cfg.consolePort];
+        };
+      })
+      config.roopkgs.system.minio;
+    # filteredCfgs;
 
     users.groups.minio = {};
 
@@ -47,51 +56,61 @@ in {
     };
 
     systemd = {
-      tmpfiles.rules = [
-        "d ${cfg.workingDirectory} 750 minio minio"
-        "d ${cfg.workingDirectory}/config 750 minio minio"
-        "d ${cfg.workingDirectory}/data 750 minio minio"
-      ];
+      tmpfiles = 
+        lib.attrsets.mapAttrs' (name: cfg: {
+          name = "rules";
+          value = [
+            "d ${cfg.workingDirectory} 750 minio minio"
+            "d ${cfg.workingDirectory}/config 750 minio minio"
+            "d ${cfg.workingDirectory}/data 750 minio minio"
+          ];
+        })
+        config.roopkgs.system.minio;
 
-      services.minio = {
-        description = "MinIO server";
-        wants = ["network-online.target"];
-        after = ["kes.service"];
+      services =
+        lib.attrsets.mapAttrs' (name: cfg: {
+          name = "minio@${name}";
+          value = {
+            description = "MinIO server";
+            wants = ["network-online.target"];
+            after = ["kes.service"];
 
-        # environment = let
-        #   kesConfigFolder = "/var/lib/kes";
-        #   caFolder = "/var/lib/minio/config/CAs";
-        # in {
-        #   SSL_CERT_DIR = kesConfigFolder;
+            # environment = let
+            #   kesConfigFolder = "/var/lib/kes";
+            #   caFolder = "/var/lib/minio/config/CAs";
+            # in {
+            #   SSL_CERT_DIR = kesConfigFolder;
 
-        #   MINIO_KMS_KES_ENDPOINT = "https://127.0.0.1:7373";
+            #   MINIO_KMS_KES_ENDPOINT = "https://127.0.0.1:7373";
 
-        #   MINIO_KMS_KES_CERT_FILE = "${caFolder}/kes.crt";
-        #   MINIO_KMS_KES_KEY_FILE = "${caFolder}/kes.key";
+            #   MINIO_KMS_KES_CERT_FILE = "${caFolder}/kes.crt";
+            #   MINIO_KMS_KES_KEY_FILE = "${caFolder}/kes.key";
 
-        #   MINIO_KMS_KES_KEY_NAME = "my-key-2";
+            #   MINIO_KMS_KES_KEY_NAME = "my-key-2";
 
-        #   MINIO_KMS_KES_CAPATH = "${kesConfigFolder}/public.crt";
-        # };
+            #   MINIO_KMS_KES_CAPATH = "${kesConfigFolder}/public.crt";
+            # };
 
-        serviceConfig = {
-          User = "minio";
-          ExecStart = ''${pkgs.minio}/bin/minio server datai --address ":${toString cfg.listenPort}" --console-address ":${toString cfg.consolePort}"'';
+            serviceConfig = {
+              User = "minio";
+              ExecStart = ''${pkgs.minio}/bin/minio server data --address ":${toString cfg.listenPort}" --console-address ":${toString cfg.consolePort}"'';
 
-          WorkingDirectory = cfg.workingDirectory;
-          Type = "simple";
-          Restart = "always";
-          TimeoutSec = 120;
-          RestartSec = 30;
-          KillMode = "process";
+              WorkingDirectory = cfg.workingDirectory;
+              Type = "simple";
+              Restart = "always";
+              TimeoutSec = 120;
+              RestartSec = 30;
+              KillMode = "process";
 
-          PrivateTmp = true;
-          PrivateDevices = true;
-          MemoryDenyWriteExecute = true;
-        };
+              PrivateTmp = true;
+              PrivateDevices = true;
+              MemoryDenyWriteExecute = true;
+            };
 
-        wantedBy = ["multi-user.target"];
+            wantedBy = ["multi-user.target"];
+          };
+        })
+        config.roopkgs.system.minio;
       };
-    };
   };
 }
