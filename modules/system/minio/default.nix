413dd3444659ee1319f37e9469d08f161f744b03
Marc Lacoursière <marc@roosoft.com>
2024-09-20 07:52:21 -0400

    minio environment variables cleanup

--- modules/system/minio/default.nix
+++ modules/system/minio/default.nix
@@ -70,21 +70,21 @@
             wants = ["network-online.target"];
             after = ["kes.service"];
 
-            # environment = let
-            #   kesConfigFolder = "/var/lib/kes";
-            #   caFolder = "/var/lib/minio/config/CAs";
-            # in {
-            #   SSL_CERT_DIR = kesConfigFolder;
+            environment = let
+              kesConfigFolder = "/var/lib/kes";
+              caFolder = "/var/lib/minio/config/CAs";
+            in {
+              SSL_CERT_DIR = kesConfigFolder;
 
-            #   MINIO_KMS_KES_ENDPOINT = "https://127.0.0.1:7373";
+              MINIO_KMS_KES_ENDPOINT = "https://127.0.0.1:7373";
 
-            #   MINIO_KMS_KES_CERT_FILE = "${caFolder}/kes.crt";
-            #   MINIO_KMS_KES_KEY_FILE = "${caFolder}/kes.key";
+              MINIO_KMS_KES_CERT_FILE = "${caFolder}/kes.crt";
+              MINIO_KMS_KES_KEY_FILE = "${caFolder}/kes.key";
 
-            #   MINIO_KMS_KES_KEY_NAME = "my-key-2";
+              MINIO_KMS_KES_KEY_NAME = "minio";
 
-            #   MINIO_KMS_KES_CAPATH = "${kesConfigFolder}/public.crt";
-            # };
+              MINIO_KMS_KES_CAPATH = "${kesConfigFolder}/public.crt";
+            };
 
             serviceConfig = {
               User = "minio";
