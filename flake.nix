b84ad90f9aec6e0fea1d02ea303b938c82a6392b
Marc Lacoursière <marc@roosoft.com>
2024-09-20 13:16:35 -0400

    removed useles inputs section

--- flake.nix
+++ flake.nix
@@ -1,10 +1,6 @@
 {
   description = "RooSoft's sane defaults for some applications";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
   outputs = {...}: {
     nixosModules = {
       home = import ./modules/home;
